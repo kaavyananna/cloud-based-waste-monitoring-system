cfb74ebc5efc189a7e086bc60e7fd79dd2b2f007
Kaavya Nannapaneni <94700095+kaavyanannapaneni@users.noreply.github.com>
2022-08-09 01:55:12 -0700

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,53 @@
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+  
+//----------- Enter you Wi-Fi Details---------//
+char ssid[] = ""; //SSID
+char pass[] = ""; // Password
+//-------------------------------------------//
+
+const int trigger = 16;
+const int echo = 5;
+long T;
+float distanceCM;
+WiFiClient  client;
+
+unsigned long myChannelField = 898169; // Channel ID
+const int ChannelField = 1; // Which channel to write data
+const char * myWriteAPIKey = "BOQS5HJNR3W0BYKI"; // Your write API Key
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+}
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin("moto","lolitsme");
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
+  }
+  digitalWrite(trigger, LOW);
+  delay(1);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  T = pulseIn(echo, HIGH);
+  distanceCM = T * 0.034;
+  distanceCM = distanceCM / 2;
+  Serial.print("Trash filled in cm: ");
+  Serial.println(distanceCM);
+  ThingSpeak.writeField(myChannelField, ChannelField, distanceCM, myWriteAPIKey);
+  delay(1000);
+}
